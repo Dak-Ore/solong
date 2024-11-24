a885b23e382faa6e45902f8c773799e024c51db4
Dak <dakore879@gmail.com>
2024-11-24 12:08:57 +0100

    trying event

---
+++ event.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   event.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsebasti <rsebasti@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 11:21:06 by rsebasti          #+#    #+#             */
+/*   Updated: 2024/11/24 11:38:54 by rsebasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx_linux/mlx.h"
+#include <stdio.h>
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	close(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	printf("Hello from key_hook! %d\n", keycode);
+	if (keycode == 65307)
+		close(vars);
+	return (0);
+}
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 640, 480, "Hello world!");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
+}
+
